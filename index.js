6226d5d0dc24857089d7f13041769a76ebdfb495
namnguyenktxd <namnguyenktxd@gmail.com>
2019-11-22 20:54:22 +0700

    update css

--- index.js
+++ index.js
@@ -8,15 +8,54 @@ var dataReceive =
 
 function onSearch() {
     const value = document.getElementById("search").value;
-    console.log(value)
+    data.map(item => {
+        const name = item.name_with_key.toLowerCase()
+        if (name === value) {
+            var html = `<div class="nentong">
+            <div class="phimtong">
+                <div class="img"><img src="${item.thumbnail}" alt="This is a picture"></div>
+                <div class="ok">
+                    <h2>${item.name_with_key}</h2>
+                    <h3>Creator: ${item.creator}</h3>
+                    <h3>National: ${item.national},Year: ${item.year}</h3>
+                    <h3>Debut_date: ${item.debut_date}</h3>
+                    <h3>Time: ${item.time}</h3>
+                    <h3>Quality: ${item.quality}</h3>
+                    <h3>Language: ${item.language}</h3>
+                </div>,
+            </div>
+            <div class="dienvien">
+            ${item.actor.map(act => {
+                return `<div class="khungdienvien">
+                <img src=${item.thumbnail} alt="actor" class="anhdienvien">
+                <h3 class="vitri">
+                    <form style="color:beige;"></form>
+                    <h4>${act}</h4>
+                </h3>
+            </div>`
+            })}
+        </div>
+            <div class="phimnho">
+                <h3>Nội dung phim</h3>
+               <h5>${item.content}</h5>
+            </div>
+        </div>`
+            container.insertAdjacentHTML("beforeend", html)
+        }
+    })
+
+    document.getElementById("all_films").style.display = "none"
 }
 
 function onLoadData(id) {
-    console.log(id)
+    if (id === "home") {
+        window.location.reload()
+    }
 }
 function filmID(id) {
     data.map(item => {
         if (id === item.key) {
+            console.log(item.actor)
             dataID = item
             var html = `<div class="nentong">
             <div class="phimtong">
@@ -32,41 +71,15 @@ function filmID(id) {
                 </div>,
             </div>
             <div class="dienvien">
-            <div class="khungdienvien">
-                <img src="pic/angelina jolie.jpg" alt="actor" class="anhdienvien">
+            ${item.actor.map(act => {
+                return `<div class="khungdienvien">
+                <img src=${item.thumbnail} alt="actor" class="anhdienvien">
                 <h3 class="vitri">
                     <form style="color:beige;"></form>
-                    <h4>${item.actor[0]}</h4>
+                    <h4>${act}</h4>
                 </h3>
-            </div>
-            <div class="khungdienvien">
-                <img src="pic/elle.jpg" alt="actor" class="anhdienvien">
-                <h3 class="vitri">
-                    <form style="color:beige;"></form>
-                    <h4>${item.actor[1]}</h4>
-                </h3>
-            </div>
-            <div class="khungdienvien">
-                <img src="pic/sharlto.jpg" alt="actor" class="anhdienvien">
-                <h3 class="vitri">
-                    <form style="color:beige,"></form>
-                    <h4>${item.actor[2]}</h4>
-                </h3>
-            </div>
-            <div class="khungdienvien">
-                <img src="pic/LESLEY.jpg" alt="actor" class="anhdienvien">
-                <h3 class="vitri">
-                    <form style="color:beige;"></form>
-                    <h4>${item.actor[3]}</h4>
-                </h3>
-            </div>
-            <div class="khungdienvien">
-                <img src="./pic/HARRIS DICKINSON.jpg" alt="actor" class="anhdienvien">
-                <h3 class="vitri">
-                    <form style="color:beige;"></form>
-                    <h4>${item.actor[4]}</h4>
-                </h3>
-            </div>
+            </div>`
+            })}
         </div>
             <div class="phimnho">
                 <h3>Nội dung phim</h3>
